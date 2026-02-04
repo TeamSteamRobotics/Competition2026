0e63ed0a49c64ed8923cd2d62aa923ca40ff2edd
Ren Brunsell <nerddragonchild@gmail.com>
2026-02-03 21:19:16 -0600

    Updated Shooter Subsystem
    Added more in-depth pseudocode to the Shooter subsystem

--- Competition2026/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ Competition2026/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -5,13 +5,75 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants;
+import frc.robot.constants.shooter;
+
 
 public class ShooterSubsystem extends SubsystemBase {
   /** Creates a new ShooterSubsystem. */
-  public ShooterSubsystem() {}
+ GenericMotor shooterLeftMotor = new TalonFXMotor(constants.shooter.shooterLeftId);
+ GenericMotor shooterRightMotor = new TalonFXMotor(constants.shooter.shooterRightId);
+ Genericmotor kickMotor = new TalonFXMotor(constants.shooter.feedRollersId);
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
+ AbsoluteEncoder shooterLeftEncoder;
+ AbsoluteEncoder shooterRightEncoder;
+
+ double m_targetSpeed;
+
+ // insert Pid code
+
+ public void StopMotor() {
+    shooterLeftMotor.set(0);  //stop left shooter wheels
+    shooterRightMotor.set(0); //stop right shooter wheels
+    kickMotor.set(0);  //stop feed roller wheels 
+ }
+
+ public boolean beamBroken() {
+    //insert beamBreak code
+ }
+ 
+ public void primeShooter(Double speed) {
+    shooterLeftMotor.set(speed);
+    shooterRightMotor.set(-speed);
+ }
+ 
+ public void runKick() {
+    runKickMotor.set(0.20);
+ }
+ 
+ public void vomit(Double speed) {
+    shooterLeftMotor.set(-speed);
+    shooterRightMotor.set(speed);
+    runKickMotor.set(-speed);
+ }
+ 
+ // need to add PIDControllers
+  public boolean Shoot(double targetSpeed) {
+    m_targetSpeed = targetSpeed;
+    if (SmartDashboard.getBoolean("Use Test Shooter Speed", false)) {
+      m_targetSpeed = SmartDashboard.getNumber("Shooter Speed",targetSpeed);
+    }
+    // Calculate how much to adjust the motor speed to reach the target.
+    double pidOutputFront = topShooterPid.calculate(shooterLeftMotor.getVelocity(), m_targetSpeed);
+    //FIXME: MAY BE THE OTHER WAY
+    double pidOutputBack = bottomShooterPid.calculate(shooterRightMotor.getVelocity(), -m_targetSpeed);
+
+    double pidGreenOutput = topShooterPid.calculate(runKickMotor.getVelocity(), m_targetSpeed);
+
+    // Set the motors to the calculated speeds.
+    shooterLeftMotor.set(-m_targetSpeed);
+    shooterRightMotor.set(-m_targetSpeed);
+
+    // Check if both motors have reached the desired speed.
+    return (topShooterPid.atSetpoint() && bottomShooterPid.atSetpoint());
   }
+ 
+ 
+ 
+ public ShooterSubsystem() {}
+
+ @Override
+ public void periodic() {
+    // This method will be called once per scheduler run
+ }
 }
