aeb15cc33b20c2f28e1001db90cfb67f28577b39
Ren Brunsell <nerddragonchild@gmail.com>
2026-03-03 19:56:23 -0600

    Fixed climb motor type
    Changed the motor from SparkMax to SparkFlex.

--- Competition2026/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ Competition2026/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -3,13 +3,18 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+
+import frc.robot.Constants;
 import frc.robot.Constants.ClimbMotors;
 import frc.robot.subsystems.Motors.GenericMotor;
 import frc.robot.subsystems.Motors.SparkMaxMotor;
 
 public class ClimbSubsystem extends SubsystemBase {
-  GenericMotor climbMotorLeft = new SparkMaxMotor(ClimbMotors.climbLeft);;
-  GenericMotor climbMotorRight = new SparkMaxMotor(ClimbMotors.climbRight);
+  SparkFlex climbMotorLeft = new SparkFlex(ClimbMotors.climbLeft, MotorType.kBrushless);
+  SparkFlex climbMotorRight = new SparkFlex(ClimbMotors.climbRight, MotorType.kBrushless);
   
   double retractClimbSpeed = ClimbMotors.climbSpeed;
 
@@ -37,11 +42,11 @@ public class ClimbSubsystem extends SubsystemBase {
 
   public boolean isRetracted() {
     // returns true if the left motor turned past the minimum turns
-    return climbMotorLeft.getAbsolutePosition() <= ClimbMotors.minClimbRotations;
+    return climbMotorLeft.getAbsoluteEncoder().getPosition() <= ClimbMotors.minClimbRotations;
   }
 
   public boolean isRaised() {
     // returns true if the left motor turned past the maximum turns
-    return climbMotorLeft.getAbsolutePosition() >= ClimbMotors.maxClimbRotations;
+    return climbMotorLeft.getAbsoluteEncoder().getPosition() >= ClimbMotors.maxClimbRotations;
   }
 }
