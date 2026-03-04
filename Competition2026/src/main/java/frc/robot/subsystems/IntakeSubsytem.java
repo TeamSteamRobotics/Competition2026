806069cb5918da64421d7822fee7012073f18a19
Ren Brunsell <nerddragonchild@gmail.com>
2026-03-03 21:20:14 -0600

    Intake PID code
    worked on pid code for intake can finish on Thursday

--- Competition2026/src/main/java/frc/robot/subsystems/IntakeSubsytem.java
+++ Competition2026/src/main/java/frc/robot/subsystems/IntakeSubsytem.java
@@ -2,14 +2,46 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+//package frc.robot.subsystems;
+
+import java.io.ObjectInputFilter.Config;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsytem extends SubsystemBase {
-  /** Creates a new IntakeSubsytem. */
-  public IntakeSubsytem() {}
+    public void runIntake(double speed){
+    m_intakeMotor.setI(speed);
+    }
+    
+    public void stopIntake(){
+      m_motor.set(0.8);
 
+    }
+
+
+    public IntakeSubsytem (){
+ Config
+      .idleMode(IdleMode.PIDValues.kP)
+      .closedLoop
+        .p(Constants.IntakeConstants.PIDValues.kP)
+        .i(Constants.IntakeConstants.PIDValues.kI)
+        .d(Constants.IntakeConstants.PIDValues.kD);
+
+        targetAngle = ConstantsIntakeConstants.intakeEncoderValue;
+        
+        IntakePivotMotor= new Sparkmax(Constants.IntakeConstants.IntakePivotMotorID, MotorType.kBrushless);
+
+        
+
+
+        
+  
+        
+
+
+
+    }
+  
+ 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
