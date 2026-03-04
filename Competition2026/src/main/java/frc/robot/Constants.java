febd25d3c9fef3851e828ea0e676f843a8ea7cb0
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2026-03-03 19:33:21 -0600

    I'm free at long last
    All 32 values added. If Heartland doesn't use the welded field layout, I'll be a bit miffed.

--- Competition2026/src/main/java/frc/robot/Constants.java
+++ Competition2026/src/main/java/frc/robot/Constants.java
@@ -75,68 +75,68 @@ public final class Constants {
           new Pose3d(new Translation3d(12.2710194, 4.638039999999999, 1.12395), 
           new Rotation3d(new Quaternion(0.7071067811865476, 0, 0, 0.7071067811865476)))))
         .chainAdd(new AprilTag(12, 
-          new Pose3d(new Translation3d(0.8511540, 0.65532, 1.4859), 
-          new Rotation3d(new Quaternion(0.8910065241883679, 0, 0, 0.45399049973954675)))))
-        .chainAdd(new AprilTag(13, 
-          new Pose3d(new Translation3d(0.851154, 7.3964799999999995, 1.4859), 
-          new Rotation3d(new Quaternion(-0.8910065241883678, 0, 0, 0.45399049973954686)))))
-        .chainAdd(new AprilTag(14, 
-          new Pose3d(new Translation3d(8.272272, 6.137656, 1.8679160000000001), 
-          new Rotation3d(new Quaternion(0, -0.25881904510252074, 0, 0.9659258262890683)))))
-        .chainAdd(new AprilTag(15, 
-          new Pose3d(new Translation3d(8.272272, 1.914906, 1.8679160000000001), 
-          new Rotation3d(new Quaternion(0, -0.25881904510252074, 0, 0.9659258262890683)))))
-        .chainAdd(new AprilTag(16, 
-          new Pose3d(new Translation3d(5.9875419999999995, -0.0038099999999999996, 1.30175), 
-          new Rotation3d(new Quaternion(0.7071067811865476, 0, 0, 0.7071067811865476)))))
-        .chainAdd(new AprilTag(17, 
-          new Pose3d(new Translation3d(4.073905999999999, 3.3063179999999996, 0.308102), 
-          new Rotation3d(new Quaternion(-0.4999999999999998, 0, 0, 0.8660254037844387)))))
-        .chainAdd(new AprilTag(18, 
-          new Pose3d(new Translation3d(3.6576, 4.0259, 0.308102), 
-          new Rotation3d(new Quaternion(0, 0, 0, 1.0)))))
-        .chainAdd(new AprilTag(19, 
-          new Pose3d(new Translation3d(4.073905999999999, 4.745482, 0.308102), 
-          new Rotation3d(new Quaternion(0.5000000000000001, 0, 0, 0.8660254037844386)))))
-        .chainAdd(new AprilTag(20, 
-          new Pose3d(new Translation3d(4.904739999999999, 4.745482, 0.308102), 
-          new Rotation3d(new Quaternion(0.8660254037844387, 0, 0, 0.49999999999999994)))))
-        .chainAdd(new AprilTag(21, 
-          new Pose3d(new Translation3d(5.321046, 4.0259, 0.308102), 
+          new Pose3d(new Translation3d(11.9528844, 7.4247756, 0.889), 
           new Rotation3d(new Quaternion(1, 0, 0, 0)))))
+        .chainAdd(new AprilTag(13, 
+          new Pose3d(new Translation3d(16.5333172, 7.4033126, 0.55245), 
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
+        .chainAdd(new AprilTag(14, 
+          new Pose3d(new Translation3d(16.5333172, 6.9715126, 0.55245), 
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
+        .chainAdd(new AprilTag(15, 
+          new Pose3d(new Translation3d(16.5329616, 4.3235626, 0.55245), 
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
+        .chainAdd(new AprilTag(16, 
+          new Pose3d(new Translation3d(16.5329616, 3.8917626, 0.55245), 
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
+        .chainAdd(new AprilTag(17, 
+          new Pose3d(new Translation3d(4.6630844, 0.6444996, 0.889), 
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
+        .chainAdd(new AprilTag(18, 
+          new Pose3d(new Translation3d(4.6256194, 3.4312351999999997, 1.12395), 
+          new Rotation3d(new Quaternion(-0.7071067811865476, -0, 0, 0.7071067811865476)))))
+        .chainAdd(new AprilTag(19, 
+          new Pose3d(new Translation3d(5.229174199999999, 3.6790375999999996, 1.12395), 
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
+        .chainAdd(new AprilTag(20, 
+          new Pose3d(new Translation3d(5.229174199999999, 4.0346376, 1.12395), 
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
+        .chainAdd(new AprilTag(21, 
+          new Pose3d(new Translation3d(4.6256194, 4.638039999999999, 1.12395), 
+          new Rotation3d(new Quaternion(0.7071067811865476, 0, 0, 0.7071067811865476)))))
         .chainAdd(new AprilTag(22, 
-          new Pose3d(new Translation3d(4.904739999999999, 3.3063179999999996, 0.308102), 
-          new Rotation3d(new Quaternion(-0.8660254037844387, 0, 0, 0.49999999999999994)))))
+          new Pose3d(new Translation3d(4.6630844, 7.4247756, 0.889), 
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
         .chainAdd(new AprilTag(23,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(4.5881798, 7.4247756, 0.889),
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
         .chainAdd(new AprilTag(24,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(4.2700194, 4.638039999999999, 1.12395),
+          new Rotation3d(new Quaternion(0.7071067811865476, 0, 0, 0.7071067811865476)))))
         .chainAdd(new AprilTag(25,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(4.0218614, 4.3902376, 1.12395),
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
         .chainAdd(new AprilTag(26,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(4.0218614, 4.0346376, 1.12395),
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
         .chainAdd(new AprilTag(27,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(4.2700194, 3.4312351999999997, 1.12395),
+          new Rotation3d(new Quaternion(-0.7071067811865476, -0, 0, 0.7071067811865476)))))
         .chainAdd(new AprilTag(28,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(4.5881798, 0.6444996, 0.889),
+          new Rotation3d(new Quaternion(6.123233995736766e-17, 0, 0, 1)))))
         .chainAdd(new AprilTag(29,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(0.0077469999999999995, 0.6659626, 0.55245),
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
         .chainAdd(new AprilTag(30,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(0.0077469999999999995, 1.0977626, 0.55245),
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
         .chainAdd(new AprilTag(31,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))))
+          new Pose3d(new Translation3d(0.0080772, 3.7457125999999996, 0.55245),
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))))
         .chainAdd(new AprilTag(32,
-          new Pose3d(new Translation3d(0, 0, 0),
-          new Rotation3d(new Quaternion(0, 0, 0, 0)))));
+          new Pose3d(new Translation3d(0.0080772, 4.1775126, 0.55245),
+          new Rotation3d(new Quaternion(1, 0, 0, 0)))));
     }
   }
 }
