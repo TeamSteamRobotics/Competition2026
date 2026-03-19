5894f71e02ec036265eea24b2b1482e296d5e7df
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2026-03-19 09:33:54 -0500

    Revert "Tuned an auto"
    This reverts commit 43ac9ab1dfe02770b09479de746a28984b8d1a9e.

--- Competition2026/src/main/java/frc/robot/Constants.java
+++ Competition2026/src/main/java/frc/robot/Constants.java
@@ -65,7 +65,7 @@ public final class Constants {
     public static final int shooterLeftId = 21;
     public static final int shooterRightId = 20;
     public static final int feedRollersId = 22;
-    public static final double defaultSpeed = 0.75;
+    public static final double defaultSpeed = 0.25;
     public static final double kickSpeed = 0.3; //0.1 filler speed fix to be accurate
     public static final double vomitSpeed = 0.60;
     public static final int indexerMotorId = 25;
