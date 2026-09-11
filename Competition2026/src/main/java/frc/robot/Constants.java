b8107335061f420f094a3b77b78f0494228ee9db
TeamSteamRobotics5119 <teamsteamrobotics@gmail.com>
2026-09-10 21:01:38 -0500

    New Slowcode set up, it shouldn't jitter, and shooter works

--- Competition2026/src/main/java/frc/robot/Constants.java
+++ Competition2026/src/main/java/frc/robot/Constants.java
@@ -65,7 +65,7 @@ public final class Constants {
     public static final int shooterLeftId = 21;
     public static final int shooterRightId = 20;
     public static final int feedRollersId = 22;
-    public static final double shootSpeed = 0.35; //.75 is default
+    public static final double shootSpeed = 0.15; //.75 is default
     public static final double kickSpeed = 0.3; //0.1 filler speed fix to be accurate
     public static final double vomitSpeed = 0.60;
     public static final int indexerMotorId = 25;
